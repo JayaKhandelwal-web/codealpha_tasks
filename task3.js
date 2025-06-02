f64c3e04d3f5b3a8a58bebac753a2602500b1c9d
JayaKhandelwal <jayakhandelwal750@gmail.com>
2025-06-02 19:00:10 +0530

    count

---
+++ task3.js
@@ -0,0 +1,13 @@
+
+function textchange(){
+let h1=document.getElementById('head1')
+h1.innerHTML="get out"
+h1.style.color="blue"
+
+}
+function textnochange(){
+let h1=document.getElementById('head1')
+h1.innerHTML="hover on me"
+h1.style.color="black"
+
+}
